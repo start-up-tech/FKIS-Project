9fe53142dbfb8157e38eead56a5b7cc90a2c1641
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-18 07:29:20 +0800

    18052021 0729
    Safwan updated generate report

---
+++ GenerateReport/DisplayReport.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<center>
+    <nav>GENERATE REPORT</nav><br>
+     <p>Report Information:<?p><br><br>
+     Report ID: <?php echo $_GET["Report_ID"]; ?><br><br>
+     Report Date: <?php echo $_GET["Report_Date"]; ?><br><br>
+     Approval Booking: <?php echo $_GET["Approval_Booking"]; ?><br><br>
+     Order ID: <?php echo $_GET["Order_ID"]; ?><br><br>
+     Audit ID: <?php echo $_GET["Audit_ID"]; ?><br><br>
+    </center>
+
+
+</body>
+</html>
---
+++ GenerateReport/GenerateReport.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generate Report</title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+<body>
+    
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "fkisdb";
+        
+        $con = mysqli_connect($servername, $username, $password, $dbname);
+
+        if(mysqli_connect_errno()){
+            echo "Failed to connect to database";
+            exit();
+        }
+        
+    ?>
+
+    <center>
+    <nav>GENERATE REPORT</nav><br>
+    <form action="DisplayReport.php" method="get">
+     Report ID: <input type="text" name="Report_ID"><br><br>
+     Report Date: <input type="date" name="Report_Date"><br><br>
+     Approval Booking: <input type="radio" id="Approve" name="Approval_Booking" value="Approve"><label for="Approve">Approve</label>
+     <input type="radio" id="Reject" name="Approval_Booking" value="Reject"><label for="Reject">Reject</label><br><br>
+     Order ID: <input type="text" name="Order_ID"><br><br>
+     Audit ID: <input type="text" name="Audit_ID"><br><br>
+     <input type="submit">
+     </form>
+    </center>
+
+    
+
+
+</body>
+</html>
