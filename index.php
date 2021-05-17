b7ec45ef9a3961268a88ff748ac9783dc6bbd3f0
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-17 13:52:12 +0800

    17052021 1352

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-    Yello
+    <nav>FKIS</nav>
+    <div id="loginPanel">
+        <form action="login.php" method="post">
+            <input type="text" name="userID" id="" placeholder="User ID">
+            <input type="password" name="userPwd" id="" placeholder="Password">
+            <input type="submit" value="Login">
+            <p>
+                Don't reuse your bank password,
+                we didn't spend a lot on security 
+                for this app 
+            </p>
+        </form>
+    </div>
+    
 </body>
 </html>
