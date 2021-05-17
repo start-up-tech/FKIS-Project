647c0f89b925a0455dcba7b2ef6aee752adfdf41
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-18 07:49:23 +0800

    18052021 0749
    Update main page.
    
    Kinda.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FKIS Online</title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+<body>
+    <nav>FKIS</nav>
+    <ul>
+        <li> <a href="./ApproveBooking/ApproveBooking.html">Approve Booking</a> </li>
+        <li> <a href="./GenerateReport/DisplayReport.php">Generate Report</a> </li>
+        <li> <a href="./Inform New Order/view.php">New Order</a> </li>
+        <li> <a href="./Login/Loginindex.php">Login</a> </li>
+        <li> <a href="./RoutineAudit/">Routine Audit</a> </li>
+       
+    </ul>
+</body>
+</html>
