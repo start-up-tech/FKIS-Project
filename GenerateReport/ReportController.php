358957484379c899e8feb3950e5969e7b42c87f2
safwan0909 <muhammadsafwan0909@gmail.com>
2021-05-25 04:02:14 +0800

    i

--- GenerateReport/ReportController.php
+++ GenerateReport/ReportController.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search</title>
+    <title>Generate Report</title>
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
