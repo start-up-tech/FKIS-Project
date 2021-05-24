34dbb9e56d3587e577333f72844d4c2f4b8a5816
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-24 14:36:56 +0800

    : ]
    Update from Safwan who don't know how to use github while having complete access to the internet. Just google how to use github

---
+++ GenerateReport/DisplayReport.php
@@ -0,0 +1,52 @@
+<?php
+    $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+    mysqli_select_db($link, "fkisdb") or die(mysqli_error($link));
+    $display = "SELECT * FROM `report`";
+    $rs_display = mysqli_query($link, $display) or die(mysqli_error($link));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+    <nav><?php include 'sidebar.php'?>FKIS</nav>
+  <center>
+    <h1><b>Report List</b></h1>
+        <table style="border: 3px solid maroon;margin:10px;padding:50px; width:60%;">
+            <thead>
+                <th colspan="5">Report ID</th>
+                <th colspan="5">Report Date</th>
+                <th colspan="5">Approval Booking</th>
+                <th colspan="5">Audit ID</th>
+                <th colspan="5">Order ID</th>
+            </thead>
+            <tbody>
+                <?php
+                    while ($row=mysqli_fetch_array($rs_display)){
+                ?>
+     
+                    <tr style="text-align:center";>
+                        <td colspan="5"><?php echo $row['Report_ID']?></td>
+                        <td colspan="5"><?php echo $row['Report_Date']?></td>
+                        <td colspan="5"><?php echo $row['Approved_Booking']?></td>
+                        <td colspan="5"><?php echo $row['Audit_ID']?></td>
+                        <td colspan="5"><?php echo $row['Order_ID']?></td>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+        <br>
+    </center>
+</body>
+</html>
+<?php
+    mysqli_close($link);
+?>
--- GenerateReport/GenerateReport.php
+++ GenerateReport/GenerateReport.php
@@ -4,41 +4,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generate Report</title>
-    <link rel="stylesheet" href="./styles/main.css">
+    <title>Search</title>
+    <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
-    
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "fkisdb";
-        
-        $con = mysqli_connect($servername, $username, $password, $dbname);
-
-        if(mysqli_connect_errno()){
-            echo "Failed to connect to database";
-            exit();
-        }
-        
-    ?>
-
-    <center>
-    <nav>GENERATE REPORT</nav><br>
-    <form action="ReportController.php" method="get">
-     Report ID: <input type="text" name="Report_ID"><br><br>
-     Report Date: <input type="date" name="Report_Date"><br><br>
-     Approval Booking: <input type="radio" id="Approve" name="Approval_Booking" value="Approve"><label for="Approve">Approve</label>
-     <input type="radio" id="Reject" name="Approval_Booking" value="Reject"><label for="Reject">Reject</label><br><br>
-     Order ID: <input type="text" name="Order_ID"><br><br>
-     Audit ID: <input type="text" name="Audit_ID"><br><br>
-     <input type="submit">
-     </form>
+    <nav><?php include 'sidebar.php'?>FKIS</nav>
+  <center>
+    <h1><b>Generate Report</b></h1>
+        <div>
+            <form action="ReportController.php" method="post">
+                <table style="border: 3px solid maroon;margin:10px;padding:50px; width:auto;">
+                    <tr>
+                        <td><label for="Report_ID">Report ID:</label></td>
+                        <td colspan="5"><input type="text" name="Report_ID"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="Report_Date">Report Date:</label></td>
+                        <td colspan="5"><input type="date" name="Report_Date"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="Approved_Booking">Approval Booking:</label></td>
+                        <td colspan="5">
+                            <input type="radio" id="Approve" name="Approved_Booking" value="Approve">
+                            <label for="Approve">Approve</label>
+                            <input type="radio" id="Reject" name="Approved_Booking" value="Reject">
+                            <label for="Reject">Reject</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="Audit_ID">Audit ID:</label></td>
+                        <td colspan="5"><input type="text" name="Audit_ID"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="Order_ID">Order ID:</label></td>
+                        <td colspan="5"><input type="text" name="Order_ID"/></td>
+                   </tr>
+                   <tr>
+                        <td>
+                            <br><input type="submit">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </center>
-
-    
-
-
 </body>
 </html>
