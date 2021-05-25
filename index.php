5398d97142187135d21adce372f52590bdce91ef
safwan0909 <muhammadsafwan0909@gmail.com>
2021-05-25 21:32:25 +0800

    k

--- index.php
+++ index.php
@@ -8,16 +8,6 @@
     <link rel="stylesheet" href="./styles/main.css">
 </head>
 <body>
-    <nav>FKIS</nav>
-    <ul>
-        <li> <a href="./ApproveBooking/Approvebooking(view).html">Approve Booking</a> </li>
-        <li> <a href="./GenerateReport/GenerateReport.php">Generate Report</a> </li>
-        <li> <a href="./InformNewOrder/view.php">New Order</a> </li>
-        <li> <a href="./Login/Loginindex.html">Login</a> </li>
-        <li> <a href="./RoutineAudit/">Routine Audit</a> </li>
-        <li> <a href="./Requestbooking/requestbooking.html">Request Booking</a> </li>
-       
-    </ul>
    <nav><?php include 'styles/Sidebar.php'?>FK Inventory System</nav><br>
    <center>
    <img src="styles/welcome.jpg" width="700" height="300" title="welcome" alt="welcome" />
