129238835033794f94662413932a558fe42b229b
AfkhAlias <84319788+AfkhAlias@users.noreply.github.com>
2021-05-25 19:06:23 +0800

    Update insertbooking.php

--- Requestbooking/insertbooking.php
+++ Requestbooking/insertbooking.php
@@ -1,38 +1,17 @@
 <?php
-	// to make a connection with database
-	$link = mysql_connect("localhost", "root") or die(mysql_error());
-
-	// to select the targeted database
-	mysql_select_db("fkisdb", $link) or die(mysql_error());
-	if(isset($_POST["submit"])) {
-
-		echo "You have entered a data";
-		//get POST Data
-		$staffid = $_POST["staffid"];
-		$item = $_POST["item"];
-		$qty = $_POST["qty"];
 	
-		$sql = "INSERT INTO booking (staffid, item, qtyr) VALUES('$staffid', '$item', '$qty') ";
-		$query = $mysqli->query($sql);
-	}
+	include("control.php");
+    extract( $_POST );
+	 
 	
-	or die(mysql_error());
-
-
-	// to run sql query in database
-	$result = mysql_query($query);
-	     
-	//Check whether the insert was successful or not
-	if($result) 
-	        {
-		        
-                    echo("Data insert");
-					
-		    }
-		else 
-	        {
-			        
-	            die("Insert failed");
-	        }
+	$query = "INSERT INTO booking (Staff_ID, Item_code, Item_Quantity) VALUES('$staffid', '$item', '$qty')";
+		
+		if (mysqli_query($conn, $query)) {
+      
+		echo "<script type='text/javascript'> window.location='readBooking.php' </script>";
+	
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
+}
 	 
 ?>
