7fcdb923fc4c5437e219834ec1774e4a9dd99d29
lookman999 <83538199+lookman999@users.noreply.github.com>
2021-06-01 00:26:18 +0800

    k

--- InformNewOrder/OrderController.php
+++ InformNewOrder/OrderController.php
@@ -47,7 +47,7 @@
                 </tr>
  
             </table><br>
-              <br><button onclick="location.href='AddOrderInterface.php'">Add Order</button><button onclick="location.href='DeleteController.php'">Delete Order</button><button onclick="location.href='ModelOrder.php'">Display List</button><br><br>
+              <br><button onclick="location.href='view.php'">Add Order</button><button onclick="location.href='DeleteController.php'">Delete Order</button><button onclick="location.href='ModelOrder.php'">Display List</button><br><br>
             <?php
             if($result) {
 		        echo("Data inserted");
