d0eefb009585091baed8f8e9cf7c361a931bf7ce
ayunibaiduri <84316719+ayunibaiduri@users.noreply.github.com>
2021-06-01 06:53:26 +0800

    Add files via upload

---
+++ Login/check.php
@@ -0,0 +1,49 @@
+<?php 
+session_start();
+
+include 'control.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$login = mysqli_query($mysqli,"select * from registration where Username='$username' and Password='$password'");
+$check = mysqli_num_rows($login);
+
+
+if($check > 0){
+
+	$data = mysqli_fetch_assoc($login);
+
+
+	if($data['level']=="admin"){
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "admin";
+		header("location:homeadmin.php");
+
+	}else if($data['level']=="staff"){
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "staff";
+		header("location:homestaff.php");
+
+
+	}else if($data['level']=="treasurer"){
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "treasurer";
+		header("location:hometreasurer.php");
+
+	}
+	else{
+
+		header("location:login2.php?login=fail");
+	}
+
+	}
+	
+
+	else{
+	header("location:login2.php?login=fail");
+}
+
+
+?>
