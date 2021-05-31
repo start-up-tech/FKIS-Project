8f8e2746849dfb62305ebb1f96a66187a810843a
ayunibaiduri <84316719+ayunibaiduri@users.noreply.github.com>
2021-06-01 06:57:31 +0800

    Add files via upload

---
+++ Login/pending.php
@@ -0,0 +1,19 @@
+<?php include('app_logic.php'); ?>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Password Recovery</title>
+	<link rel="stylesheet" href="main2.css">
+</head>
+<body>
+
+	<form class="login-form" action="login.php" method="post" style="text-align: center;">
+		<p>
+			We sent an email to  <b><?php echo $_GET['email'] ?></b> to help you recover your account. 
+		</p>
+	    <p>Please login into your email account and click on the link we sent to reset your password</p>
+	</form>
+	<p><center><p><a href="login2.php">BACK TO LOGIN PAGE</a></</center></p>	
+</body>
+</html>
