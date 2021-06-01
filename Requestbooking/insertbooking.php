4604547ccae872ff7b8e988238a8330d86d90772
AfkhAlias <84319788+AfkhAlias@users.noreply.github.com>
2021-06-01 08:59:53 +0800

    Update insertbooking.php

--- Requestbooking/insertbooking.php
+++ Requestbooking/insertbooking.php
@@ -4,11 +4,10 @@
     extract( $_POST );
 	 
 	
-	$query = "INSERT INTO booking (Staff_ID, Item_code, Item_Quantity) VALUES('$staffid', '$item', '$qty')";
+	$query = "INSERT INTO booking (Staff_ID, Item_code, Item_Quantity) VALUES('$Staff_ID', '$item', '$qty')";
 		
 		if (mysqli_query($conn, $query)) {
-      
-		echo "<script type='text/javascript'> window.location='readBooking.php' </script>";
+		echo "<script type='text/javascript'> window.location='viewitem.php' </script>";
 	
 } else {
     echo "Error: " . $query . "<br>" . mysqli_error($conn);
