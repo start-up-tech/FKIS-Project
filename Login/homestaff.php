fa16a99e673f177e319e4686293314285afa3872
ayunibaiduri <84316719+ayunibaiduri@users.noreply.github.com>
2021-06-06 00:49:39 +0800

    Add files via upload

---
+++ Login/homestaff.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FKIS Online</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+   <nav>FK Inventory System</nav>
+   <nav id="barlist"> 
+		<div class="topnav">
+		<a class="active">Home</a>
+  		<a href="#">Sample</a>
+  		<a href="#">Sample</a>
+  		<a href="#">Report</a>
+
+	<div class ="topnav-right">
+		<a href="logout.php">Logout</a>
+	</div>
+	</div>
+	</nav>
+	<h3>REQUEST BOOKING</h3>
+		&emsp; <a href="reqbooking.php"><img src="reqbooking.jpg" width="400" height="200" /></a><br><br><br>
+	
+</body>
+</html>	
