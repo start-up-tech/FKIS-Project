c2c2a78ff107d42f0926e250de478e62a990c657
ayunibaiduri <ayunibaiduri07@gmail.com>
2021-06-13 18:40:33 +0800

    edit

--- Login/homeadmin.php
+++ Login/homeadmin.php
@@ -12,72 +12,6 @@ include("control.php"); ?>
 </head>
    <nav><?php include '../styles/sidebaradmin.php'?>FK Inventory System</nav>
 </head>
-   <style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Style the header */
-header {
-    background-color:hsl(0, 79%, 40%);
-  padding: 30px;
-  text-align: center;
-  font-size: 35px;
-  color: black;
-}
-
-/* Container for flexboxes */
-section {
-  display: -webkit-flex;
-  display: flex;
-}
-
-/* Style the navigation menu */
-b {
-  -webkit-flex: 1;
-  -ms-flex: 1;
-  flex: 1;
-  background-color:hsl(0, 0%, 85%);
-  padding: 20px;
-}
-
-/* Style the list inside the menu */
-nav ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-/* Style the content */
-article {
-  -webkit-flex: 3;
-  -ms-flex: 3;
-  flex: 3;
-  background-color: #f1f1f1;
-  padding: 10px;
-  color: black;
-}
-
-/* Style the footer */
-footer {
-    background-color:hsl(0, 0%, 95%);
-  padding: 10px;
-  text-align: center;
-  color: white;
-}
-
-/* Responsive layout - makes the menu and the content (inside the section) sit on top of each other instead of next to each other */
-@media (max-width: 600px) {
-  section {
-    -webkit-flex-direction: column;
-    flex-direction: column;
-  }
-}
-</style>
-</head>
 <body onload="getDateTime()">
 <?php
 	$servername = "localhost";
@@ -91,10 +25,6 @@ footer {
     }
     
     $sql = "SELECT * FROM login"; ?>
-
-<header>
-  <h2>Welcome to the Admin page of FKIS System :]</h2>
-</header><br>
 
 <section>
   <b>
