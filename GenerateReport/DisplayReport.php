710a1820c73a0ba924f20bf705fdad74a1f7addc
ayunibaiduri <ayunibaiduri07@gmail.com>
2021-06-13 08:48:01 +0800

    Update DisplayReport.php

--- GenerateReport/DisplayReport.php
+++ GenerateReport/DisplayReport.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
-    <nav>FK Inventory System</nav>
+<nav><?php include '../styles/sidebaradmin.php'?>FK Inventory System</nav>
   <center>
     <h1><b>Report List</b></h1>
         <table style="border: 3px solid maroon;margin:10px;padding:50px; width:60%;">
