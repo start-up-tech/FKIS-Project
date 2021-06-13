045af7f5246519dc6eb96f821ebb27099f6cabcd
ayunibaiduri <ayunibaiduri07@gmail.com>
2021-06-13 09:26:43 +0800

    Update homeadmin.php

--- Login/homeadmin.php
+++ Login/homeadmin.php
@@ -9,6 +9,38 @@
 </head>
 <body>
    <nav><?php include '../styles/sidebaradmin.php'?>FK Inventory System</nav>
+
+   </head>
+<body>
+
+<h2>CSS Layout Float</h2>
+<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
+<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
+
+<header>
+  <h2>Cities</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">London</a></li>
+      <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+</section>
+
+<footer>
+  <p>Footer</p>
+</footer>
+
 	&emsp; <a href=""><img src="images/audit.png" width="400" height="200" />  </a> 
 	&emsp; <a href=""><img src="images/APPBOOKING.JPG" width="400" height="200" /></a>
 	&emsp; <a href=""><img src="images/report.jpg" width="400" height="200" /></a><br><br><br>
