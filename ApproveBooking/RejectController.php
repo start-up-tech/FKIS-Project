5c4497bbe300864dfe2f9deccd8328c529ebf032
asmyra <83567063+asmyra@users.noreply.github.com>
2021-06-14 12:56:36 +0800

    Update RejectController.php

--- ApproveBooking/RejectController.php
+++ ApproveBooking/RejectController.php
@@ -4,5 +4,5 @@ $rowCount = count($_POST["booking"]);
 for($i=0;$i<$rowCount;$i++) {
 mysqli_query($conn, "UPDATE booking SET Booking_Status='Rejected' WHERE Booking_ID='" . $_POST["booking"][$i] . "'");
 }
-header("Location:booking.php");
+header("Location:ViewBookingInterface.php");
 ?>
