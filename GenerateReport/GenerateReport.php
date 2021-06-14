55efcdbd40a13254f6fa7b4132ca7a8123add6ea
safwan0909 <muhammadsafwan0909@gmail.com>
2021-06-14 11:00:06 +0800

    kk

--- GenerateReport/GenerateReport.php
+++ GenerateReport/GenerateReport.php
@@ -24,15 +24,15 @@
                     </tr>
                     <tr>
                         <td><label for="Approved_Booking">Approved Booking:</label></td>
-                        <td colspan="5"><input type="text" name="Approved_Booking" required/></td>
+                        <td colspan="5"><input type="text" name="Approved_Booking"/></td>
                     </tr>
                     <tr>
                         <td><label for="Audit_ID">Audit ID:</label></td>
-                        <td colspan="5"><input type="text" name="Audit_ID" required/></td>
+                        <td colspan="5"><input type="text" name="Audit_ID"/></td>
                     </tr>
                     <tr>
                         <td><label for="Order_ID">Order ID:</label></td>
-                        <td colspan="5"><input type="text" name="Order_ID" required/></td>
+                        <td colspan="5"><input type="text" name="Order_ID"/></td>
                    </tr>
                    <tr>
                         <td>
